8098c4bfb1c8fe0a39030e24782d18210f9d2a9d
leo-trevisol <leotrevisol.2021@gmail.com>
2025-07-10 14:40:02 -0300

    add firebase config

--- KotlinComposeLearning/app/build.gradle.kts
+++ KotlinComposeLearning/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -58,5 +59,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.1")
-
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    implementation ("com.google.firebase:firebase-firestore-ktx")
 }
